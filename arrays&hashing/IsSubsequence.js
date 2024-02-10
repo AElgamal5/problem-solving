2a8a69c3c5e33b89cf66c39daf4f2c8d1514b00e
Ahmed Elgamal <ahmedelgamal5320@gmail.com>
2024-02-10 09:15:09 +0200

    Is Subsequence

---
+++ arrays&hashing/IsSubsequence.js
@@ -0,0 +1,39 @@
+/**
+ * 392. Is Subsequence
+ * -----------------------
+ * link: https://leetcode.com/problems/is-subsequence/description/
+ */
+
+//---------------------------------------------------------------
+//------------------------1st trial------------------------------
+//---------------------------------------------------------------
+//space: O()
+//time: O()
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function (s, t) {
+  //optional
+  if (s === t) return true;
+  if (s.length > t.length) return false;
+  //--------
+
+  let p = 0;
+  for (let i = 0; i < t.length; i++) {
+    if (s[p] === t[i]) {
+      p++;
+    }
+  }
+  if (p === s.length) {
+    return true;
+  }
+  return false;
+};
+
+console.log(isSubsequence("ace", "abcdef")); //true
+console.log(isSubsequence("abc", "ahbgdc")); //true
+console.log(isSubsequence("axc", "ahbgdc")); //false
+console.log("=".repeat(30));
