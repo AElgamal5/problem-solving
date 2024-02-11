c2ee9ee82a9224da12ddab8b952305b294bf4bb5
Ahmed Elgamal <ahmedelgamal5320@gmail.com>
2024-02-11 21:59:45 +0200

    Length of Last Word

---
+++ arrays&hashing/lengthOfLastString.js
@@ -0,0 +1,49 @@
+/**
+ * 392. Length of Last String
+ * -----------------------
+ * link: https://leetcode.com/problems/length-of-last-word/
+ */
+
+//---------------------------------------------------------------
+//------------------------1st trial------------------------------
+//---------------------------------------------------------------
+//space: O(n)
+//time: O(n)
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  return s.trim().split(" ").slice(-1)[0].length;
+};
+
+// console.log(lengthOfLastWord("Hello World")); //5
+// console.log(lengthOfLastWord("   fly me   to   the moon  ")); //4
+// console.log(lengthOfLastWord("luffy is still joyboy")); //6
+// console.log(lengthOfLastWord("test 123456")); //6
+// console.log(lengthOfLastWord("=".repeat(30))); //6
+
+//---------------------------------------------------------------
+//------------------------2st trial------------------------------
+//---------------------------------------------------------------
+//space: O(1)
+//time: O(n)
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord2 = function (s) {
+  let ans = 0;
+  for (let i = s.length - 1; i > -1; i--) {
+    if (s[i] === " " && ans > 0) {
+      break;
+    }
+    if (s[i] !== " ") {
+      ans++;
+    }
+  }
+  return ans;
+};
+
+console.log(lengthOfLastWord2(" 123456  ")); //6
