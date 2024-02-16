60ae6c8ec96d08a87fa7e4fad37e8c8f60422d2b
Ahmed Elgamal <ahmedelgamal5320@gmail.com>
2024-02-16 08:28:20 +0200

    Isomorphic Strings done

--- arrays&hashing/isomorphicStrings.js
+++ arrays&hashing/isomorphicStrings.js
@@ -9,9 +9,11 @@
 //---------------------------------------------------------------
 
 //solution description:
+//saving each unique char of s and its equivalent  char  of 't'
+//same for t
 
 //space: O(2n)
-//time: O(n^2)
+//time: O(n)
 
 /**
  * @param {string} s
@@ -22,10 +24,26 @@ var isIsomorphic = function (s, t) {
   let sMap = new Map();
   let tMap = new Map();
 
-  for (let i = 0; i < s.length; i++) {}
+  for (let i = 0; i < s.length; i++) {
+    let sChar = s[i];
+    let tChar = t[i];
+
+    if (
+      (sMap.has(sChar) && sMap.get(sChar) != tChar) ||
+      (tMap.has(tChar) && tMap.get(tChar) != sChar)
+    ) {
+      return false;
+    }
+
+    sMap.set(sChar, tChar);
+    tMap.set(tChar, sChar);
+  }
+
+  return true;
 };
 
-// console.log(isIsomorphic("egg", "add"));
-// console.log(isIsomorphic("foo", "bar"));
-// console.log(isIsomorphic("paper", "title"));
-console.log(isIsomorphic("abab", "baba"));
+// console.log(isIsomorphic("egg", "add")); //true
+// console.log(isIsomorphic("foo", "bar")); //false
+// console.log(isIsomorphic("paper", "title")); //true
+// console.log(isIsomorphic("abab", "baba")); //true
+// console.log(isIsomorphic("bbbaaaba", "aaabbbba")); //false
