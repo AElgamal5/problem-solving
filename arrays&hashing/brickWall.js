00ab0845aedd3bde329df0298460b1c68d5b5a2a
Ahmed Elgamal <ahmedelgamal5320@gmail.com>
2024-03-22 00:37:05 +0200

    Brick Wall

---
+++ arrays&hashing/brickWall.js
@@ -0,0 +1,55 @@
+/**
+ * 554. Brick Wall
+ * -----------------------
+ * link: https://leetcode.com/problems/brick-wall/
+ */
+
+//---------------------------------------------------------------
+//------------------------1st trial------------------------------
+//---------------------------------------------------------------
+
+//description:
+//1- make map for counting the gaps [key => position, value => no. gaps in this pos]
+//2- find the pos where maximum no. gaps
+//3- return no. rows of wall - max no. gaps
+
+//time: O(n^2)
+//space: O(n)
+
+/**
+ * @param {number[][]} wall
+ * @return {number}
+ */
+var leastBricks = function (wall) {
+  const gapsMap = new Map();
+
+  for (let i = 0; i < wall.length; i++) {
+    let pos = 0;
+    for (let j = 0; j < wall[i].length - 1; j++) {
+      pos += wall[i][j];
+      gapsMap.set(pos, (gapsMap.get(pos) || 0) + 1);
+    }
+  }
+
+  let max = 0;
+  gapsMap.forEach((value, key) => {
+    if (value > max) {
+      max = value;
+    }
+  });
+
+  return wall.length - max;
+};
+
+console.log(
+  leastBricks([
+    [1, 2, 2, 1],
+    [3, 1, 2],
+    [1, 3, 2],
+    [2, 4],
+    [3, 1, 2],
+    [1, 3, 1, 1],
+  ])
+);
+
+console.log(leastBricks([[1], [1], [1]]));
